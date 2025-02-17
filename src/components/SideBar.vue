98704529f46fa9a747089cc90b698883e7818bb8
rferrel74 <rferrel74@gmail.com>
2025-02-16 17:22:10 -0800

    Next Commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="sidebar">
+        <h2>Menu Lateral</h2>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>
+            <li><router-link to="/lista-tareas">Lista de Tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar{
+    width: 200px;
+    background-color: rgb(220, 217, 217);
+    padding: 15px;
+}
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="container">
+    <Sidebar></Sidebar>
+      <div class="contenido">
+        <RouterView></RouterView>
+      </div>
+   </div>
+</template>
+
+<script setup>
+import Sidebar from '../components/SideBar.vue';
+import { RouterView } from 'vue-router';
+</script>
+
+<style>
+.container{
+    display: flex;
+}
+.contenido{
+    flex: 1;
+    padding: 20px;
+}
+</style>
